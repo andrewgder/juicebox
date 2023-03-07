002001255e69fed7183eea452293c5121288b517
Andrew Der <andrew.g.der@gmail.com>
2023-03-06 21:21:03 -0500

    completed servers part 1

---
+++ index.js
@@ -0,0 +1,17 @@
+// inside index.js
+const PORT = 3000;
+const express = require("express");
+const server = express();
+const morgan = require("morgan");
+server.use(morgan("dev"));
+const { client } = require("./db");
+client.connect();
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
