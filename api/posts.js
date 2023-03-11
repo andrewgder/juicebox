856a79d76efa96c2150f9f9347ca15029a97d2b4
Andrew Der <andrew.g.der@gmail.com>
2023-03-11 15:33:51 -0500

    server part 3 complete

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,7 @@
 // api/posts.js
 const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts, createPost } = require("../db");
+const { getAllPosts, createPost, updatePost, getPostById } = require("../db");
 
 const { requireUser } = require("./utils");
 
@@ -37,11 +37,100 @@ postsRouter.use((req, res, next) => {
 
   next(); // THIS IS DIFFERENT
 });
-postsRouter.get("/", async (req, res) => {
-  const posts = await getAllPosts();
-  res.send({
-    posts,
-  });
+// postsRouter.get("/", async (req, res) => {
+//   const posts = await getAllPosts();
+//   res.send({
+//     posts,
+//   });
+// });
+
+postsRouter.get("/", async (req, res, next) => {
+  try {
+    const allPosts = await getAllPosts();
+
+    const posts = allPosts.filter((post) => {
+      // keep a post if it is either active, or if it belongs to the current user
+      if (post.active) {
+        return true;
+      }
+
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+
+      // none of the above are true
+      return false;
+    });
+
+    res.send({
+      posts,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+postsRouter.patch("/:postId", requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost });
+    } else {
+      next({
+        name: "UnauthorizedUserError",
+        message: "You cannot update a post that is not yours",
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+//deactivates posts
+postsRouter.delete("/:postId", requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(
+        post
+          ? {
+              name: "UnauthorizedUserError",
+              message: "You cannot delete a post which is not yours",
+            }
+          : {
+              name: "PostNotFoundError",
+              message: "That post does not exist",
+            }
+      );
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 // api/tags.js
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -13,6 +13,36 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  console.log("what im sending", req.params.tagName);
+  let { tagName } = req.params;
+  tagName = decodeURIComponent(tagName);
+  try {
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+    const allPosts = await getPostsByTagName(tagName);
+    const posts = allPosts.filter((post) => {
+      // keep a post if it is either active, or if it belongs to the current user
+      if (post.active) {
+        return true;
+      }
+
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+
+      // none of the above are true
+      return false;
+    });
+
+    res.send({ posts });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
